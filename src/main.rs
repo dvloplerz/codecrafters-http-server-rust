07c0d0610366c9a14762f9a39702f2431a783301
dvloplerz <ittipat135@gmail.com>
2023-10-25 20:25:22 +0700

    Passed `Get a file` chapter.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+#![allow(unused)]
+use std::fs;
+use std::io::BufReader;
+use std::path::Path;
 // Uncomment this block to pass the first stage
 // use std::net::TcpListener;
-use std::net::TcpListener;
+use std::{net::TcpListener, env};
 
 use crate::handler::handle_connection;
 use std::thread::spawn;
+use std::string::String;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,27 +16,49 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221")
         .expect("Cannot bind to Address or Port.");
 
+    let args: Vec<_> = std::env::args().collect();
+    let dir: Directory = if args.len() > 1 && args[1].contains("directory") {
+        Directory {
+            path: args[2].to_string(),
+        }
+    } else {
+        Directory {
+            path: "".to_string(),
+        }
+    };
+
     for stream in listener.incoming() {
         let stream = stream.expect("TcpStream Error.");
+        let dir = dir.path.to_string();
 
-        spawn(move || handle_connection(stream));
+        spawn(move || handle_connection(dir, stream));
     }
 }
 
+pub struct Directory {
+    path: String,
+}
+
 pub mod handler {
+    use crate::Directory;
     use std::{
         io::{prelude::*, BufReader},
         net::TcpStream,
+        path::Path,
+        fs, env,
     };
-    use crate::{http_status::HttpStatus, response::Response};
+    use crate::{
+        http_status::HttpStatus,
+        response::{Response, ContentType},
+    };
 
-    pub fn handle_connection(mut stream: TcpStream) {
+    pub fn handle_connection(dir: String, mut stream: TcpStream) {
         let buf_reader = BufReader::new(&mut stream);
-        let http_request: Vec<_> =
-            buf_reader.lines()
-                      .map(|result| result.unwrap())
-                      .take_while(|line| !line.is_empty())
-                      .collect();
+        let http_request: Vec<_> = buf_reader
+            .lines()
+            .map(|result| result.unwrap())
+            .take_while(|line| !line.is_empty())
+            .collect();
 
         let mut request_clone = http_request.clone();
         if request_clone.is_empty() {
@@ -47,17 +74,18 @@ pub mod handler {
 
         match extract_path(path)[0] {
             "" => {
-                Response::new(
-                    HttpStatus::OK,
-                    Some("This is home!".to_string()),
-                )
-                .send(&mut stream);
+                Response::new(HttpStatus::OK, ContentType::Plain, None)
+                    .send(&mut stream);
             }
             "echo" => {
                 let response_body =
                     extract_path(path)[1..].join("/").to_string();
-                Response::new(HttpStatus::OK, Some(response_body))
-                    .send(&mut stream);
+                Response::new(
+                    HttpStatus::OK,
+                    ContentType::Plain,
+                    Some(response_body),
+                )
+                .send(&mut stream);
             }
             "user-agent" => {
                 let user_agent = request_clone
@@ -68,10 +96,39 @@ pub mod handler {
                     .split_whitespace()
                     .collect::<Vec<_>>()[1]
                     .to_string();
-                Response::new(HttpStatus::OK, Some(user_agent))
-                    .send(&mut stream);
+                Response::new(
+                    HttpStatus::OK,
+                    ContentType::Plain,
+                    Some(user_agent),
+                )
+                .send(&mut stream);
             }
-            _ => Response::new(HttpStatus::NotFound, None).send(&mut stream),
+            "files" => {
+                let path = &extract_path(path)[1..];
+                let file_path = format!("{}{}", dir, path.join("/"));
+                let file = Path::new(&file_path);
+
+                if file.exists() {
+                    let response = std::fs::read_to_string(file)
+                        .unwrap_or("Cannot Read file.".to_string());
+
+                    Response::new(
+                        HttpStatus::OK,
+                        ContentType::OctetStream,
+                        Some(response.to_string()),
+                    )
+                    .send(&mut stream);
+                } else {
+                    Response::new(
+                        HttpStatus::NotFound,
+                        ContentType::Plain,
+                        None,
+                    )
+                    .send(&mut stream)
+                }
+            }
+            _ => Response::new(HttpStatus::NotFound, ContentType::Plain, None)
+                .send(&mut stream),
         }
     }
 
@@ -111,17 +168,41 @@ mod http_status {
 
 pub mod response {
     use std::{net::TcpStream, io::Write};
-
     use crate::http_status::HttpStatus;
+
+    pub enum ContentType {
+        Plain,
+        OctetStream,
+    }
+
+    impl std::fmt::Display for ContentType {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let content_type = match &self {
+                Self::Plain => "text/plain",
+                Self::OctetStream => "application/octet-stream",
+            };
+
+            write!(f, "{content_type}")
+        }
+    }
 
     pub struct Response {
         http_status: HttpStatus,
+        response_type: ContentType,
         body: Option<String>,
     }
 
     impl Response {
-        pub fn new(http_status: HttpStatus, body: Option<String>) -> Self {
-            Self { http_status, body }
+        pub fn new(
+            http_status: HttpStatus,
+            response_type: ContentType,
+            body: Option<String>,
+        ) -> Self {
+            Self {
+                http_status,
+                response_type,
+                body,
+            }
         }
 
         pub fn send(&self, stream: &mut TcpStream) {
@@ -132,11 +213,14 @@ pub mod response {
 
             let body_length = body.len();
 
-            let response =
-                format!("HTTP/1.1 {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", self.http_status, body_length, body);
+            let response = format!(
+                "HTTP/1.1 {}\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+                self.http_status, self.response_type, body_length, body
+            );
 
-            stream.write_all(response.as_bytes())
-                  .expect("Cannot Write to stream.");
+            stream
+                .write_all(response.as_bytes())
+                .expect("Cannot Write to stream.");
         }
     }
 }
