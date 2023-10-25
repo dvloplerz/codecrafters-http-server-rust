f2bf67113ba0e8aebb75483a5f9ae4fc1390ea9d
dvloplerz <ittipat135@gmail.com>
2023-10-25 14:14:12 +0700

    Rewrite workflow.

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,114 @@
+// Uncomment this block to pass the first stage
+// use std::net::TcpListener;
 use std::net::TcpListener;
 
-// Uncomment this block to pass the first stage
-use request::request;
+use crate::handler::handle_connection;
 
 fn main() {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    request(listener).ok();
-}
+    let listener = TcpListener::bind("127.0.0.1:4221")
+        .expect("Cannot bind to Address or Port.");
 
-pub mod error {
-    pub enum Error {
-        InvalidRequest,
-        InvalidProtocol,
-        InvalidMethod,
-        IO(String),
-        Utf8(String),
-    }
+    for stream in listener.incoming() {
+        let stream = stream.expect("TcpStream Error.");
 
-    impl From<std::str::Utf8Error> for Error {
-        fn from(value: std::str::Utf8Error) -> Self {
-            Self::Utf8(value.to_string())
-        }
-    }
-
-    impl From<std::io::Error> for Error {
-        fn from(value: std::io::Error) -> Self {
-            Self::IO(value.to_string())
-        }
+        handle_connection(stream);
     }
 }
 
-pub mod method {
-    use std::str::FromStr;
-    use crate::error::Error;
+pub mod handler {
+    use std::{
+        io::{prelude::*, BufReader},
+        net::TcpStream,
+    };
+    use crate::{http_status::HttpStatus, response::Response};
 
-    #[derive(Debug)]
-    pub enum Method {
-        Get,
-        Post,
-        Put,
-        Delete,
-    }
+    pub fn handle_connection(mut stream: TcpStream) {
+        let buf_reader = BufReader::new(&mut stream);
+        let http_request: Vec<_> =
+            buf_reader.lines()
+                      .map(|result| result.unwrap())
+                      .take_while(|line| !line.is_empty())
+                      .collect();
 
-    impl FromStr for Method {
-        type Err = Error;
-        fn from_str(value: &str) -> Result<Self, Self::Err> {
-            match value {
-                "GET" => Ok(Method::Get),
-                "POST" => Ok(Method::Post),
-                "PUT" => Ok(Method::Put),
-                "DELETE" => Ok(Method::Delete),
-                _ => Err(Error::InvalidMethod),
+        let request_clone = http_request.clone();
+
+        let _request_info =
+            extract_request(request_clone.first().unwrap().as_str());
+
+        let _method = _request_info.0;
+        let path = _request_info.1;
+
+        match extract_path(path)[0] {
+            "" => {
+                Response::new(
+                    HttpStatus::OK,
+                    Some("This is home!".to_string()),
+                )
+                .send(&mut stream);
             }
+            "echo" => {
+                let response_body =
+                    extract_path(path)[1..].join("/").to_string();
+                Response::new(HttpStatus::OK, Some(response_body))
+                    .send(&mut stream);
+            }
+            "user-agent" => {
+                let user_agent = request_clone
+                    .iter()
+                    .filter(|v| v.contains("User-Agent"))
+                    .collect::<Vec<_>>()[0]
+                    .as_str()
+                    .split_whitespace()
+                    .collect::<Vec<_>>()[1]
+                    .to_string();
+                Response::new(HttpStatus::OK, Some(user_agent))
+                    .send(&mut stream);
+            }
+            _ => Response::new(HttpStatus::NotFound, None).send(&mut stream),
         }
+    }
+
+    pub fn extract_request(request_info: &str) -> (&str, &str) {
+        let mut request = request_info.split_whitespace();
+
+        let method = request.next().unwrap_or("GET");
+        let path = request.next().unwrap_or("/");
+
+        (method, path)
+    }
+
+    pub fn extract_path(path: &str) -> Vec<&str> {
+        let path = path.split('/').collect::<Vec<_>>();
+        path[1..].to_vec()
     }
 }
 
-pub mod http_status {
-    use std::fmt::Display;
-
-    #[derive(Debug)]
+mod http_status {
     pub enum HttpStatus {
         OK = 200,
         BadRequest = 400,
         NotFound = 404,
     }
 
-    impl Display for HttpStatus {
-        // add code here
+    impl std::fmt::Display for HttpStatus {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            let message = match self {
+            let message = match &self {
                 Self::OK => "200 OK",
-                Self::BadRequest => "400 Bad Request",
+                Self::BadRequest => "400 BadRequest",
                 Self::NotFound => "404 Not Found",
             };
-            write!(f, "{}", message)
+            write!(f, "{message}")
         }
     }
 }
 
 pub mod response {
-    use std::fmt::Display;
-    use std::net::TcpStream;
-    use std::io::Write;
+    use std::{net::TcpStream, io::Write};
+
     use crate::http_status::HttpStatus;
 
-    #[derive(Debug)]
     pub struct Response {
         http_status: HttpStatus,
         body: Option<String>,
@@ -97,80 +119,19 @@ pub mod response {
             Self { http_status, body }
         }
 
-        pub fn send(&self, stream: &mut TcpStream) -> std::io::Result<()> {
+        pub fn send(&self, stream: &mut TcpStream) {
             let body = match &self.body {
-                Some(v) => v,
+                Some(body) => body,
                 None => "",
             };
-            let body_len = body.len();
-            write!(stream,
-                   "HTTP/1.1 {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                   self.http_status, body_len, body)
+
+            let body_length = body.len();
+
+            let response =
+                format!("HTTP/1.1 {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", self.http_status, body_length, body);
+
+            stream.write_all(response.as_bytes())
+                  .expect("Cannot Write to stream.");
         }
-    }
-
-    impl Display for Response {
-        // add code here
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            let content = "";
-            write!(f, "Response::Display:: {}", &content)
-        }
-    }
-}
-
-pub mod request {
-    use std::{net::TcpListener, io::Read};
-    use crate::{method::Method, http_status::HttpStatus};
-    use crate::response::Response;
-    use crate::error::Error;
-
-    pub struct Request {
-        method: Method,
-    }
-
-    impl Request {
-        pub fn method(&self) -> &Method {
-            &self.method
-        }
-    }
-
-    pub fn request(listener: TcpListener) -> Result<(), Error> {
-        let mut buffer: [u8; 1024] = [0; 1024];
-
-        for stream in listener.incoming() {
-            let mut stream = stream?;
-            let _ = stream.read(&mut buffer);
-            let buffers = std::str::from_utf8(&buffer)?;
-
-            let mut headers = buffers.split("\r\n");
-
-            let mut sp_str = headers.next().unwrap().split_whitespace();
-            let _method: Method =
-                sp_str.next().ok_or(Error::InvalidMethod)?.parse()?;
-            let _path = sp_str.next().unwrap();
-
-            let _host = headers.next().unwrap();
-
-            let _user_agent = headers.next().unwrap();
-
-            if _path == "/" {
-                let _ = Response::new(HttpStatus::OK, None).send(&mut stream);
-            } else if _path.contains("/echo") {
-                let echo =
-                    _path.split("/echo/").collect::<Vec<_>>()[1].to_string();
-
-                let _ =
-                    Response::new(HttpStatus::OK, Some(echo)).send(&mut stream);
-            } else if _path == "/user-agent" {
-                let ua = _user_agent.split_whitespace().collect::<Vec<_>>()[1]
-                    .to_string();
-                let _ =
-                    Response::new(HttpStatus::OK, Some(ua)).send(&mut stream);
-            } else {
-                let _ =
-                    Response::new(HttpStatus::NotFound, None).send(&mut stream);
-            }
-        }
-        Ok(())
     }
 }
